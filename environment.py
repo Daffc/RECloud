e2a8caa9c890ee8f4f789563f20de750ab7dff08
Douglas Affonso Clementino <dac17@inf.ufpr.br>
2021-02-10 12:24:11 -0300

    Redefinind method for ip recovering from virtual machines. Defining sheband in python scripts.

--- environment.py
+++ environment.py
@@ -1,8 +1,10 @@
+#!/usr/bin/python3
+
 import subprocess
 import json
 import os.path
 import socket
-
+import collections
 
 #initiating json structure
 data = {}
@@ -16,7 +18,7 @@ hostname = socket.gethostname()
 data['hostname'] = hostname;
 
 
-#Recovering IP from the host machine [TODO]
+#Recovering IP from the host machine
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect(("8.8.8.8", 80))
 data['ip'] = s.getsockname()[0];
@@ -28,11 +30,9 @@ p2 = subprocess.Popen(('grep', 'qemu'), stdin = p1.stdout, stdout=subprocess.PIP
 saida_procs = subprocess.check_output(("grep", "-v", "grep"), stdin=p2.stdout)
 
 
-#Recovering information about all the NIC's in the system
-p1 = subprocess.Popen(['arp -n'], stdout = subprocess.PIPE, shell=True)
-nics = p1.communicate()[0].decode('utf-8')
-
-
+#Recovering information about the subnet of host_machine (data['ip']).
+p1 = subprocess.Popen(['sudo', 'nmap', '-sn', data['ip'][:data['ip'].rfind(".")] +'.0/24'], stdout = subprocess.PIPE)
+subnet = p1.communicate()[0].decode('utf-8')
 data['machines'] = []
 
 #iterating over each VM process entry, recovering its informaion
@@ -44,16 +44,19 @@ for line in saida_procs.splitlines():
   vm_name = line_split[7]
   uuid = line_split[20]
 
-  #Recovering virtual network of the system into 'saida_vir_net'
+  #Recovering MAC address of virtual machine 'vm_name'
   p1 = subprocess.Popen(['virsh domiflist ' + vm_name], stdout = subprocess.PIPE, shell=True)
   output = p1.communicate()[0].decode('utf-8')
   mac_address = output.splitlines()[2].split()[4] 
   
-  #Iterating over Network Interfaces, retriving 'ip' by the 'mac_adress' of the current virtual machine.
-  for entry in nics.split('\n'):
-    if mac_address in entry:
-      ip = entry.split()[0]
-  
+  #Iterating over machines in subnet, retriving 'ip' by the 'mac_adress' of the current virtual machine.
+  cache = collections.deque([], 2)
+  for entry in subnet.split('\n'):
+    if mac_address.upper() in entry: 
+      ip = cache[1].split()[4]
+    cache.appendleft(entry)
+
+  # Storing definind new VM entry in 'data'.
   data['machines'].append({
     'name': vm_name,
     'PID': pid,
@@ -62,7 +65,7 @@ for line in saida_procs.splitlines():
     'ip': ip  
   })	  
 
-
+# Storing all the 'data' information in 'envirionment.json'
 with open('./monitoring/environment.json', 'w') as env_file:
     json.dump(data, env_file)
-
+#
