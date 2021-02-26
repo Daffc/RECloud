e6837a1f31969c0c58588e64f0549a855dee395e
Douglas A. C <dac17@inf.ufpr.br>
2021-02-26 11:59:30 -0300

    Verifying sudo permission before executing python3.9 install

--- set_python3.9.sh
+++ set_python3.9.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run this script with sudo privileges."
+  exit
+fi
+
 apt update && apt upgrade 
 apt install wget build-essential libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev 
 wget https://www.python.org/ftp/python/3.9.1/Python-3.9.1.tgz -P/tmp/
