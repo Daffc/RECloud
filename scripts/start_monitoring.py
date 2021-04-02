f46ce3f93cdf403075368a4dc0ecda2f13e198d2
Douglas Affonso Clementino <dac17@inf.ufpr.br>
2021-04-02 11:54:04 -0300

    Apllying relative paths and renaming 'monitoring.py' to star_monitoring.py'

--- scripts/start_monitoring.py
+++ scripts/start_monitoring.py
@@ -1,20 +1,33 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.9
 
 import os
 import signal
 import subprocess
 
+#=============================
+#   Some General Definitions
+#=============================
+PROGRAM_PATH = os.path.dirname(os.path.abspath(__file__))
+
+
+
+
+# Cheking if this program is running under supdo provileges.
+if(os.getuid() != 0):
+  print("Please run this script with sudo privileges.")
+  exit(1)
+
 # Checking if the monitorinf folder exists.
-if(not os.path.isdir('./monitoring')):
+if(not os.path.isdir(f'{PROGRAM_PATH}/monitoring')):
   # Creating monitoring folder
-  subprocess.call(['mkdir', 'monitoring'])
+  subprocess.call(['mkdir', f'{PROGRAM_PATH}/monitoring'])
 
 # Defining output-file from top execution
-top_output = open("./monitoring/top_output.txt", "w+")
+top_output = open(f'{PROGRAM_PATH}/monitoring/top_output.txt', "w+")
 
 # Calling 'top' and 'iptraf-ng' processes.
 top = subprocess.Popen(["top","-u", "libvirt-qemu", "-d", "1", "-b"], stdout = top_output)
-network = subprocess.Popen(["iptraf", "-u", "-i", "all", "-B", "-L", "./monitoring/network_output.txt"])
+network = subprocess.Popen(["iptraf", "-u", "-i", "all", "-B", "-L", f'{PROGRAM_PATH}/monitoring/network_output.txt'])
 
 
 # Waiting for users input to finish the monitoring process
