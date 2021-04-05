4f01671cf2a64268e7bc1706db6c4a3c7ec17066
Douglas Affonso Clementino <dac17@inf.ufpr.br>
2021-04-05 17:01:32 -0300

    For some reason, when calling 'sig_help.py' from '../libs/sig_helper.py' the program 'star_monitoring.py' does not execute properlly when remotelly called by 'start_all_monitoring.py'

---
+++ scripts/monitoring_scripts/sig_helper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3.9
+
+import signal
+
+# Handling the SIGTERM signal.
+class GracefulKiller:
+  kill_now = False
+  def __init__(self):
+    signal.signal(signal.SIGINT, self.exit_gracefully)
+    signal.signal(signal.SIGTERM, self.exit_gracefully)
+
+  def exit_gracefully(self,signum, frame):
+    self.kill_now = True
+
