53e766ae2152c6fb362d5cd5ef921de7e08ae559
Douglas Affonso Clemtnino <dac17@inf.ufpr.br>
2021-05-27 15:45:25 -0300

    Recovering list ov virtual machines from hosts. Cross connecting all virtual machines. Removing recv_environments.py

--- scripts/workloads_scripts/NAS/set_all_environments.py
+++ scripts/workloads_scripts/NAS/set_all_environments.py
@@ -2,6 +2,8 @@
 
 import os
 import sys
+import json
+import shutil
 import setproctitle
 import argparse
 
@@ -44,6 +46,37 @@ def setAllEnvironments(clients, user, password):
   RemoteCommand(clients,f'. {VENV_PATH} && echo \'{user}\n{password}\nN\'| {SET_ENV_PATH}', 10, False).remoteCommandHandler()
   print('OK!', flush=True)
 
+def outputVMList(clients, origin, destination):
+ 
+  # Receiving environment files from 'clients'
+  helper.receiveFiles(clients, f'{PROGRAM_PATH}/../../data/environment.json', f'{PROGRAM_PATH}/environments/')
+
+  print(f'Listing VM from {clients.hosts}...', flush=True)
+  
+  envirnment_files = os.listdir(origin)
+  with open(destination,"w") as output_file:
+    for ef in envirnment_files:
+      print(f'\t from \'{ef}\':')
+      with open(f'{origin}/{ef}', "r") as environment_file:
+        env = json.load(environment_file)
+        for vm in env["virtualMachines"]:
+          print(f'\t\t \'{vm["ip"]}\'')
+          output_file.write(f'{vm["ip"]}\n')
+
+  print('OK!', flush=True)
+  
+  print(f'Removing Temporary files \'{PROGRAM_PATH}/environments/\'', flush=True)
+  shutil.rmtree(f'{PROGRAM_PATH}/environments/')
+  print('OK!', flush=True)
+
+
+def crossConnectAll(user, password, vm_file_path):
+
+  vm_hosts = helper.recoverHosts(f'{vm_file_path}', FileType.TEXT)
+  vm_clients = helper.defineConnection(user, password, vm_hosts)
+  
+  helper.crossConnect(vm_hosts, vm_clients)
+
 # Parsing program initialization arguments. 
 def parsingArguments():
     parser = argparse.ArgumentParser()
@@ -67,4 +100,10 @@ if __name__ == "__main__":
   clients = helper.defineConnection(user, password, hosts)
   
   # Run script 'set_environment.py' for all 'clients'
-  setAllEnvironments(clients, user, password)
+  #setAllEnvironments(clients, user, password)
+
+  # Listing Virtual Machines from 'clients' Files
+  outputVMList(clients, f'{PROGRAM_PATH}/environments', f'{PROGRAM_PATH}/VM_list.txt')
+
+  # Cross connecting all Virtual Machines
+  crossConnectAll(user, password, f'{PROGRAM_PATH}/VM_list.txt')
