eab55ea2318dff98e9736d3d1d50fc290e50d523
Douglas A.C <dac17@inf.ufpr.br>
2021-09-10 13:42:05 -0300

    Calling tcpdump for node network monitoring over bridge (br0).

--- scripts/monitoring_scripts/startMonitoring.py
+++ scripts/monitoring_scripts/startMonitoring.py
@@ -17,6 +17,7 @@ PROGRAM_PATH = os.path.dirname(os.path.abspath(__file__))
 DATA_FOLDER_PATH = os.path.normpath(f'{PROGRAM_PATH}/../data')
 CPU_MEM_OUTPUT_FILE = os.path.normpath(f'{DATA_FOLDER_PATH}/cpu_mem_output.txt')
 NETWORK_OUTPUT_FILE = os.path.normpath(f'{DATA_FOLDER_PATH}/network_output.txt')
+TCPDUMP_OUTPUT_FILE = os.path.normpath(f'{DATA_FOLDER_PATH}/network_output.pcap')
 VENV_PATH =  os.path.normpath(f'{PROGRAM_PATH}/../../venv/bin/python3.9')
 
 #=============================
@@ -72,6 +73,7 @@ if __name__ == "__main__":
   # Calling 'top' and 'iptraf-ng' processes.
   cpu_mem = subprocess.Popen([VENV_PATH, f"{PROGRAM_PATH}/cpuMemMonitor.py", "-o", CPU_MEM_OUTPUT_FILE])
   network = subprocess.Popen([f"{PROGRAM_PATH}/iptraf-ng/iptraf-ng", "-i", "all", "-B", "-L", NETWORK_OUTPUT_FILE])
+  tcpdump = subprocess.Popen([f"tcpdump", "-i", "br0", "-s", "96", "-w", TCPDUMP_OUTPUT_FILE])
   
   print(f'Monitoring environment with  "cpuMemMonitor.py" (PID=[{cpu_mem.pid}]) and iptraf-ng (PID=[{network.pid}, {network.pid + 1}])...')
   killer = GracefulKiller()
@@ -83,12 +85,14 @@ if __name__ == "__main__":
   #(NOTE) When called, "iptraf-ng" creates two processes, while the first becomes <defunct> the second does the traffic monitoring.
   os.kill((network.pid + 1), signal.SIGUSR2)
   cpu_mem.terminate()
+  tcpdump.terminate()
 
   # Avoiding subprocesses to remain as zombies.
   cpu_mem.communicate()
   network.communicate()
+  tcpdump.communicate()
 
   # Wainting for subprocesses conclusion.
-  waitSubprocessesDie([cpu_mem.pid, (network.pid + 1)])
+  waitSubprocessesDie([cpu_mem.pid, (network.pid + 1), tcpdump.pid])
   
   print(f'Exiting monitoring processes halder.')
