e4939af9290f4c6e91faa8f7f5f89a66cea933a6
Douglas Affonso Clementino <dac17@inf.ufpr.br>
2021-10-01 15:05:30 -0300

    Replacing 'print_time' for 'stringifyTimespec' to return the time readable string instead of directly print it. Calculating time lapse between two timespec structures in 'timespec_enlapse'. Converting  timespec structure to double in timespecToDouble.

--- scripts/monitoring_scripts/C_scripts/timeLib.c
+++ scripts/monitoring_scripts/C_scripts/timeLib.c
@@ -3,12 +3,71 @@
 #include <string.h>
 #include <time.h>
 
-void print_time(struct timespec timestamp){
+#include "timeLib.h"
+
+// Returns pointer to human-readable string of "timestamp".
+char * stringifyTimespec(struct timespec timestamp){
     time_t now = timestamp.tv_sec;
     struct tm ts;
-    char buf[100];
-    // Format time, "ddd yyyy-mm-dd hh:mm:ss"
+    static char buf[100];
+
     ts = *localtime(&now);
     strftime(buf, sizeof(buf), "%a %Y-%m-%d %H:%M:%S", &ts);
-    printf("%s.%09ld\n", buf, timestamp.tv_nsec);
+    sprintf(buf + strlen (buf), ".%lu", timestamp.tv_nsec);
+
+    return buf;
+}
+
+// Calculates time enlapse between from timespec 'x' to 'y', storing difference in 'result', and returning singal of operation.
+int timespec_enlapse (struct timespec *result, struct timespec x, struct timespec  y){
+  
+    struct timespec * big;
+    struct timespec * sml;
+
+    int nsec;
+    unsigned long long a, b;
+    int signal;
+
+
+    // Checking which of the values to the largest by adjusting the "big" and "sml" pointers and the "signal" return sign.
+    a = (x.tv_sec * BILLION) + x.tv_nsec;
+    b = (y.tv_sec * BILLION) + y.tv_nsec;
+
+    if(a < b){
+        big = &y;
+        sml = &x; 
+        signal = 1;
+    }
+    else{
+        big = &x;
+        sml = &y;
+        signal = -1;
+    }
+
+    // Adjusting nanoseconds to execute the subtraction.
+    if (big->tv_nsec < sml->tv_nsec) {
+        nsec = (sml->tv_nsec - big->tv_nsec) / BILLION + 1;
+        sml->tv_nsec -= BILLION * nsec;
+        sml->tv_sec += nsec;
+    }
+    if (big->tv_nsec - sml->tv_nsec > BILLION) {
+        nsec = (big->tv_nsec - sml->tv_nsec) / BILLION;
+        sml->tv_nsec += BILLION * nsec;
+        sml->tv_sec -= nsec;
+    }
+
+    result->tv_sec = big->tv_sec - sml->tv_sec;
+    result->tv_nsec = big->tv_nsec - sml->tv_nsec;
+
+    /* Return signal of the elapsed time. */
+    return signal;
+}
+
+
+// Receiver a timespec 't_time' and returns correspondent double value.
+double timespecToDouble(struct timespec *t_time){
+    double d_time;
+
+    d_time = t_time->tv_sec + (t_time->tv_nsec * 1.0 / BILLION);
+    return d_time;
 }
--- scripts/monitoring_scripts/C_scripts/timeLib.h
+++ scripts/monitoring_scripts/C_scripts/timeLib.h
@@ -4,7 +4,15 @@
 #ifndef __TIMELIB__
 #define __TIMELIB__
 
-    // Prints the human-readable value of "timestamp".
-    void print_time(struct timespec timestamp);
+    #define BILLION 1000000000
+
+    // Returns pointer to human-readable string of "timestamp".
+    char * stringifyTimespec(struct timespec timestamp);
+
+    // Calculates time enlapse between from timespec 'x' to 'y', storing difference in 'result', and returning singal of operation.
+    int timespec_enlapse (struct timespec *result, struct timespec x, struct timespec  y);
+
+    // Receiver a timespec 't_time' and returns correspondent double value.
+    double timespecToDouble(struct timespec *t_time);
 
 #endif
