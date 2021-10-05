d1a7a4c2409106caff3369a4454466f630c3faef
Douglas A.C <dac17@inf.ufpr.br>
2021-10-05 13:39:16 -0300

    Defining function 'calculateNextInterval'  to calculate the next interval between samplings (nanosleep) according to the accumulated errors between previous measurements.

--- scripts/monitoring_scripts/C_scripts/timeLib.h
+++ scripts/monitoring_scripts/C_scripts/timeLib.h
@@ -10,9 +10,16 @@
     char * stringifyTimespec(struct timespec timestamp);
 
     // Calculates time enlapse between from timespec 'x' to 'y', storing difference in 'result', and returning singal of operation.
-    int timespec_enlapse (struct timespec *result, struct timespec x, struct timespec  y);
+    int timespecElapsed (struct timespec *result, struct timespec x, struct timespec  y);
 
-    // Receiver a timespec 't_time' and returns correspondent double value.
+    // Receive a timespec 't_time' and returns correspondent double value.
     double timespecToDouble(struct timespec *t_time);
+
+    // Receive a double '*d_time' and returns the correspondent timespec.
+    struct timespec doubleToTimespec(double *d_time);
+
+    // Calculates the difference between measures time samplings (ts_actual to ts_prev), 
+    // retuning new adjusted time interval according to the desired interval in 'db_delay', and accumulated error in 'db_acc'. 
+    struct timespec calculateNextInterval(struct timespec ts_actual, struct timespec ts_prev, double db_delay, double *db_acc);
 
 #endif
