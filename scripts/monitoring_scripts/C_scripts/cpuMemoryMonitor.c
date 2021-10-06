35b9998194afd1083ad85d5f790fe58c2d7539bb
Douglas A.C <dac17@inf.ufpr.br>
2021-10-06 19:44:13 -0300

    Defining parsing of arguments: '-h' informing help message, '-o' redirecting the output of the program to a file,  '-d' redefining desired time delay between samplings.

--- scripts/monitoring_scripts/C_scripts/cpuMemoryMonitor.c
+++ scripts/monitoring_scripts/C_scripts/cpuMemoryMonitor.c
@@ -4,12 +4,15 @@
 #include <string.h>
 #include <signal.h>
 #include <libvirt/libvirt.h>
-
-#include <unistd.h>
+#include <ctype.h>
 
 #include "cpuMemoryMonitor.h"
 #include "timeLib.h"
 #include "signals.h"
+
+#define MAX_DELAY 5
+#define MIN_DELAY 0.05
+#define DEFAULT_DELAY 0.5
 
 // Attempting to connect with virtual environment informed by 'connection'. 
 virConnectPtr libvirtConnect(const char * connection){
@@ -118,23 +121,102 @@ void setBalloonPeriod(TDomain *domain, int period){
     }
 }
 
+// Reads Arguments received by the program, informing help messages, 
+// defining the output of the program and/or redefining the desired delay between samples.
+void parseArguments(int argc, char *argv[], FILE **output, double *delay){
+    char c;
+    
+    // Defining variables with default values.
+    *delay = DEFAULT_DELAY;
+    *output = stdout;
+
+    while ((c = getopt (argc, argv, "ho:d:")) != -1)
+    switch (c){
+        // Helper Argument.
+        case 'h': 
+            // Outputting help message.
+            printf(
+                "Program to collect CPU and Memory data from Virtual Machines. Displayed data will follow the schema for each time interval:\n"
+                "\tTIMESTAMP; [VM0]; [VM1]; [VM2] ...\n"
+                "Each [VMN] entry will have the following Information:\n"
+                "\tVM-NAME VM-MEM-ABS VM-MEM-PERC VM-CPU-PERC.\n\n"
+                "Optional arguments:\n"
+                "\t-h\tDisplay help Message.\n"
+                "\t-o\tPath to output file. If not informed, data will be displayed in stdout.\n"
+                "\t-d\tThe time interval between sampling the statistics in seconds (must bee between %d and %.2f, default is %.1f).\n",
+                MAX_DELAY,
+                MIN_DELAY,
+                DEFAULT_DELAY
+            );
+
+            exit(0);
+            break;
+
+        // Redefine delay between samples (default 0.5s).
+        case 'd':
+            // Converting value that follows '-d' to double, storing in *delay and, in case of error, print error and exists.
+            if(sscanf(optarg, "%lf", delay) != 1){
+                fprintf(stderr, "ERROR: value '%s' does not represents time in seconds (must be between %d and %.2lf).\n", optarg, MAX_DELAY, MIN_DELAY);
+                exit(1);
+            }
+            else 
+                if( (*delay < MIN_DELAY )|| (*delay > MAX_DELAY) ){
+                    fprintf(stderr, "ERROR: value '%s' out of boundaries (must be between %d and %.2lf).\n", optarg, MAX_DELAY, MIN_DELAY);
+                    exit(1);
+                }
+
+            break;
+        
+        // Redefnine output of program to a file. 
+        case 'o':
+            
+            // Defining file descriptor to the output file. 
+            *output = fopen(optarg,"w");
+
+            // Checking if the operation was successful.
+            if (*output == NULL) {
+                fprintf(stderr, "ERROR: Error while oppening file '%s'.\n", optarg);
+                exit(1);
+            }
+            break;
+
+        // Unknow argument / not optinal argument not informed.  
+        case '?':
+            if (optopt == 'd')
+                fprintf (stderr, "ERROR: Option '-d' requires an argument in seconds between %d and %.2f (default is 0.5).\n", MAX_DELAY, MIN_DELAY);
+            else if (optopt == 'o')
+                fprintf (stderr, "ERROR: Option '-o' requires an argument path to output file.\n");
+            else if (isprint (optopt))
+                fprintf (stderr, "ERROR: Unknown option '-%c'.\n", optopt);
+            else
+                fprintf (stderr, "ERROR: Unknown option character '\\x%x'.\n", optopt);
+            exit(1);
+
+        // Fallback.
+        default:
+            abort();
+    }
+
+}
 int main(int argc, char *argv[])
 {
     struct timespec ts_sampling;
     virConnectPtr conn;
     TDomainsList domains;
-    volatile sig_atomic_t * exit;
+    volatile sig_atomic_t * terminate;
+    FILE * output;
 
     double db_delay;
     double db_acc;
-    struct timespec interval;
+    struct timespec ts_interval;
     struct timespec ts_prev;
 
+    // Parsing Arguments (Defining interval between samples in 'delay' and output file descriptor in 'output').
+    parseArguments(argc, argv, &output, &db_delay);
 
     // Initiating time control variables.
     db_acc = 0;                                 // The variable that accumulates error between sampling rounds.
-    db_delay = 0.5;                             // The desirable delay between sampling rounds.
-    interval = doubleToTimespec(&db_delay);     // The Timespec representation of delay between sampling intervals.
+    ts_interval = doubleToTimespec(&db_delay);     // The Timespec representation of delay between sampling intervals.
 
     // Establishing connection with qemu.
     conn = libvirtConnect("qemu:///system");
@@ -149,14 +231,14 @@ int main(int argc, char *argv[])
     }
 
     // Recover pointer to SIGTER signal handler.
-    exit =  startGracefullExiting();
+    terminate =  startGracefullExiting();
 
     // Initializing variable to calculate elapsed time.
     clock_gettime(CLOCK_REALTIME, &ts_prev);
-    nanosleep(&interval , &interval);
+    nanosleep(&ts_interval , &ts_interval);
 
     // Stops loop when receive SIGTER.
-    while(!(*exit)){
+    while(!(*terminate)){
 
         // Sampling current time.
         clock_gettime(CLOCK_REALTIME, &ts_sampling);
@@ -168,25 +250,25 @@ int main(int argc, char *argv[])
         }
 
         // Printing sampling time.
-        printf("%s", stringifyTimespec(ts_sampling));
+        fprintf(output, "%s", stringifyTimespec(ts_sampling));
         
         // For all domains, dusplay statistics.
         for(int i = 0; i < domains.number; i ++){
-            printf("; %s", domains.list[i].name);               // Domain name.
-            printf(" %llu", domains.list[i].used_mem);          // Domain used memory (used + cached in KB).
-            printf(" %3.2f", domains.list[i].used_mem_perc);    // Domain used memory percentage (used + cached).
-            printf(" %3.2f", domains.list[i].cpu_perc);         // Domain used memory percentage.
+            fprintf(output, "; %s", domains.list[i].name);               // Domain name.
+            fprintf(output, " %llu", domains.list[i].used_mem);          // Domain used memory (used + cached in KB).
+            fprintf(output, " %3.2f", domains.list[i].used_mem_perc);    // Domain used memory percentage (used + cached).
+            fprintf(output, " %3.2f", domains.list[i].cpu_perc);         // Domain used memory percentage.
         }
-        printf("\n");
+        fprintf(output,"\n");
 
         // Calculating new adusted interval.
-        interval = calculateNextInterval(ts_sampling, ts_prev, db_delay, &db_acc);
+        ts_interval = calculateNextInterval(ts_sampling, ts_prev, db_delay, &db_acc);
 
         // Updating value of previous time measured (ts_prev).
         ts_prev = ts_sampling;
         
         // Sleeping
-        nanosleep(&interval , &interval);
+        nanosleep(&ts_interval , &ts_interval);
     }
 
     // Deactivatind Balloon for all domains.
@@ -196,4 +278,7 @@ int main(int argc, char *argv[])
 
     // Closing connection with the virtualizer.
     virConnectClose(conn);
+
+    // Closing file descriptor of output.
+    fclose(output);
 }
