a27855c157038d0fca91d0cb9e862e7ce7add04e
Douglas Affonso Clemtnino <dac17@inf.ufpr.br>
2022-06-29 16:33:34 -0300

    Defining runners for experiments.

---
+++ scripts/experiments/runLoad.sh
@@ -0,0 +1,127 @@
+#/bash/bin
+
+# RECOVERING VMS IPs to vms file
+# cat /tmp/experiment_28-06-2022_15\:45\:42/environments.json  | json_pp | grep "virtual_machines" -A 10 | grep "ip" | cut -d : -f 2 | cut -d \" -f 2
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+EXPERIMENTS_DELAYS=(0.1 0.2 0.5 1 2)
+EXPERIMENT_TIMES=$1
+EXPERIMENT_DURATION=$2
+
+
+# CHECKING IF EXISTS WITH ERROR, ENDING THE PROCESS.
+function checkComandReturn() {
+    if ! $*; then 
+        printf "\n${RED}ERROR:${NC} While executing:\n"
+        printf "\t$*\n"
+        exit
+    fi
+}
+
+function CheckFile() {
+    if [[ ! -f $* ]];then
+        echo "File '$*' needs to be in current directory" 
+        exit 
+    fi
+}
+
+function checkCreateFolders(){
+    if [[ ! -d "results" ]];then
+        mkdir results;
+    fi
+
+    EXP_DATE=LOAD_$(date +'%Y-%m-%d_%T');
+    OUT_MAIN_FOLDER="results/$EXP_DATE";
+    if [[ ! -d "$OUT_MAIN_FOLDER" ]]; then 
+        mkdir "$OUT_MAIN_FOLDER";
+    fi
+    retval=$OUT_MAIN_FOLDER
+
+}
+
+function createExperimentFolder(){
+    if [[ ! -d "$1" ]]; then 
+        mkdir "$1";
+    fi
+}
+function checkArguments(){
+    if ! [ "$2" -eq "$2" ] 2> /dev/null; then
+        printf "\n${RED}ERROR:${NC} Invalid value for '$1'\n";
+        exit 1;
+    fi
+}
+
+main() {
+    
+    # CHECKING PROGRAM ARGUMENTS
+    checkArguments EXPERIMENT_TIMES $EXPERIMENT_TIMES;
+    checkArguments EXPERIMENT_DURATION $EXPERIMENT_DURATION;
+    printf "*************************************************\n";
+    printf "EXECUTING EXPERIMENTS FOR:\n";
+    printf "\tEXPERIMENT_TIMES: $EXPERIMENT_TIMES\n";
+    printf "\tEXPERIMENT_DURATION: $EXPERIMENT_DURATION\n";
+    printf "\tEXPERIMENTS_DELAYS: ${EXPERIMENTS_DELAYS[*]}\n";
+    printf "*************************************************\n";
+    
+
+    checkCreateFolders
+    OUT_MAIN_FOLDER=$retval
+
+    for delay in "${EXPERIMENTS_DELAYS[@]}"; do
+        createExperimentFolder "$OUT_MAIN_FOLDER/$delay"
+    done;
+
+    echo "" > logLoad.txt;
+
+    # CHECKING FILES
+    CheckFile "credentials"
+    CheckFile "nodos"
+    CheckFile "vms"
+
+    # GETTING CREDENTIALS
+    printf "GETTING CREDENTIALS... "
+    USER=$(head -n 1 credentials) 
+    PASSWORD=$(tail -n 1 credentials)
+
+    echo "[$USER] [$PASSWORD]"
+    printf "OK\n" 
+
+
+    # SETTING CORES IN 'performance' MODE
+    printf "SETTING NODES IN PERFORMANCE..."
+    checkComandReturn parallel-ssh -h nodos -i "echo $PASSWORD| sudo -S  cpupower frequency-set -g performance" >> logLoad.txt 2>> logLoad.txt;
+    checkComandReturn parallel-ssh -h nodos -i "cpufreq-info |  grep 'frequency is'" >> logLoad.txt 2>> logLoad.txt;
+    printf "OK\n" 
+    
+
+    # START STRESS IN VIRTUAL MACHINES
+    printf "Starting CPU stress to all cores of all virtual machines..."
+    checkComandReturn parallel-ssh -h vms -i "nohup stress-ng --matrix 0 -t 0 > /dev/null 2>&1 &" >> logLoad.txt 2>> logLoad.txt;
+    printf "OK\n" 
+
+
+    printf "***** RUNNING EXPERIMENTS *****\n"
+    checkComandReturn . ~/tg_scripts/venv/bin/activate >> logLoad.txt 2>> logLoad.txt;
+
+    for delay in "${EXPERIMENTS_DELAYS[@]}"; do
+        for (( t=1; t<=$EXPERIMENT_TIMES; t++ )); do
+            printf "\tITERATION (DELAY:$delay\tITERATION:$t)\n"
+            checkComandReturn ~/tg_scripts/scripts/monitoring_scripts/startAllMonitoring.py nodos "$OUT_MAIN_FOLDER/$delay" -d $delay -t $EXPERIMENT_DURATION >> logLoad.txt 2>> logLoad.txt <./credentials;
+        done;
+    done;
+    printf "***** FINISHING EXPERIMENTS *****\n"
+
+    printf "Stopping CPU stress to all cores of all virtual machines..."
+    checkComandReturn parallel-ssh -h vms -i "killall stress-ng" >> logLoad.txt 2>> logLoad.txt;
+    printf "OK\n" 
+
+    # SETTING CORES IN 'ondemand' MODE
+    printf "SETTING NODES IN ONDEMAND... "
+    checkComandReturn parallel-ssh -h nodos -i "echo $PASSWORD| sudo -S  cpupower frequency-set -g ondemand" >> logLoad.txt 2>> logLoad.txt;
+    checkComandReturn parallel-ssh -h nodos -i "cpufreq-info |  grep 'frequency is'" >> logLoad.txt 2>> logLoad.txt;
+    printf "OK\n" 
+
+}
+main
+
